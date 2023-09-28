266428f77093bde2dae55bc221eb6a59d51b94b9
Daniel McNeela <daniel.mcneela@gmail.com>
2023-09-28 11:01:23 -0500

    start on general manifold encoder

--- models/encoders.py
+++ models/encoders.py
@@ -1,6 +1,6 @@
 """Graph encoders."""
 import manifolds
-import layers.hyp_layers as hyp_layers
+import layers.manifold_layers as manifold_layers
 import numpy as np
 import torch
 import torch.nn as nn
@@ -17,9 +17,7 @@ class Encoder(nn.Module):
         self.c = c
 
     def encode(self, x, adj):
-        print("X: ", x)
         if self.encode_graph:
-            print('yolo')
             input = (x, adj)
             output, _ = self.layers.forward(input)
         else:
@@ -43,21 +41,21 @@ class MLP(Encoder):
         self.encode_graph = False
 
 
-class HNN(Encoder):
+class ManifoldNN(Encoder):
     """
-    Hyperbolic Neural Networks.
+    Manifold Neural Network.
     """
     def __init__(self, c, args):
         super(HNN, self).__init__(c)
         self.manifold = getattr(manifolds, args.manifold)()
         assert args.num_layers > 1
-        dims, acts, _ = hyp_layers.get_dim_act_curv(args)
+        dims, acts, _ = manifold_layers.get_dim_act_curv(args)
         hnn_layers = []
         for i in range(len(dims) - 1):
             in_dim, out_dim = dims[i], dims[i + 1]
             act = acts[i]
             hnn_layers.append(
-                hyp_layers.HNNLayer(
+                manifold_layers.ManifoldNNLayer(
                     self.manifold, in_dim, out_dim, self.c, args.dropout, act, args.bias
                 )
             )
@@ -66,7 +64,7 @@ class HNN(Encoder):
 
     def encode(self, x, adj):
         x_hyp = self.manifold.proj(self.manifold.expmap0(self.manifold.proj_tan0(x, self.c), c=self.c), c=self.c)
-        return super(HNN, self).encode(x_hyp, adj)
+        return super().encode(x_hyp, adj)
 
 class GCN(Encoder):
     """
@@ -83,58 +81,39 @@ class GCN(Encoder):
             gc_layers.append(GraphConvolution(in_dim, out_dim, args.dropout, act, args.bias))
         self.layers = nn.Sequential(*gc_layers)
         self.encode_graph = True
-
-
-class HypGCN(Encoder):
+    
+class ManifoldGCN(Encoder):
     """
-    Hyperbolic-GCN.
+    A GCN on an arbitrary manifold.
     """
     def __init__(self, c, args):
         super().__init__(c)
-        if args.manifold not in ["Spherical", "Euclidean", "PoincareBall", "Hyperboloid"]:
-            manifold_array = []
-            word = list(args.manifold)
-            for i in range(0,len(word), 2):
-                if word[i] == "E":
-                    manifold_name = "Euclidean"
-                elif word[i] == "P":
-                    manifold_name = "PoincareBall"
-                elif word[i] == "S":
-                    manifold_name = "Spherical"
-                elif word[i] == "H":
-                    manifold_name = "Hyperboloid"
-                else:
-                    raise ValueError("Invalid string in the manifold")
-                count = int(word[i + 1])
-                manifold_array.append((getattr(manifolds, manifold_name)(),count))
-            self.manifold_name = "Product"
-            self.manifold = getattr(manifolds, self.manifold_name)(manifold_array)
-
-        else:
-            self.manifold = getattr(manifolds, args.manifold)()
-        
+        self.manifold = getattr(manifolds, args.manifold)()
         assert args.num_layers > 1
-        dims, acts, self.curvatures = hyp_layers.get_dim_act_curv(args)
+        dims, acts, self.curvatures = manifold_layers.get_dim_act_curv(args)
         self.curvatures.append(self.c)
-        hgc_layers = []
+        layers = []
         for i in range(len(dims) - 1):
             c_in, c_out = self.curvatures[i], self.curvatures[i + 1]
-            print('C in', c_in, 'C out', c_out)
             in_dim, out_dim = dims[i], dims[i + 1]
             act = acts[i]
-            hgc_layers.append(
-                hyp_layers.HyperbolicGraphConvolution(
-                    self.manifold, in_dim, out_dim, c_in, c_out, args.dropout, act, args.bias, args.use_att
+            layers.append(
+                manifold_layers.ManifoldGraphConvolution(
+                    self.manifold,
+                    in_dim, out_dim, 
+                    c_in, c_out, 
+                    args.dropout, 
+                    act, args.bias, 
+                    args.use_att, args.local_agg
                 )
             )
-        self.layers = nn.Sequential(*hgc_layers)
+        self.layers = nn.Sequential(*layers)
         self.encode_graph = True
 
     def encode(self, x, adj):
-        x_hyp = x
-        # x_hyp = self.manifold.proj(
-        #         self.manifold.expmap0(self.manifold.proj_tan0(x, self.curvatures[0]), c=self.curvatures[0]),
-        #         c=self.curvatures[0])
+        x_tan = self.manifold.proj_tan0(x, self.curvatures[0])
+        x_hyp = self.manifold.expmap0(x_tan, c=self.curvatures[0])
+        x_hyp = self.manifold.proj(x_hyp, c=self.curvatures[0])
         return super().encode(x_hyp, adj)
 
 
@@ -143,7 +122,7 @@ class GAT(Encoder):
     Graph Attention Networks.
     """
     def __init__(self, c, args):
-        super(GAT, self).__init__(c)
+        super().__init__(c)
         assert args.num_layers > 0
         dims, acts = get_dim_act(args)
         gat_layers = []
@@ -154,7 +133,8 @@ class GAT(Encoder):
             out_dim = dims[i + 1] // args.n_heads
             concat = True
             gat_layers.append(
-                    GraphAttentionLayer(in_dim, out_dim, args.dropout, act, args.alpha, args.n_heads, concat))
+                GraphAttentionLayer(in_dim, out_dim, args.dropout, act, args.alpha, args.n_heads, concat)
+            )
         self.layers = nn.Sequential(*gat_layers)
         self.encode_graph = True
 
@@ -165,7 +145,7 @@ class Shallow(Encoder):
     Learns embeddings or loads pretrained embeddings and uses an MLP for classification.
     """
     def __init__(self, c, args):
-        super(Shallow, self).__init__(c)
+        super().__init__(c)
         self.manifold = getattr(manifolds, args.manifold)()
         self.use_feats = args.use_feats
         weights = torch.Tensor(args.n_nodes, args.dim)
@@ -197,36 +177,4 @@ class Shallow(Encoder):
         h = self.lt[self.all_nodes, :]
         if self.use_feats:
             h = torch.cat((h, x), 1)
-        return super(Shallow, self).encode(h, aui)
-
-
-class ProdGCN(Encoder):
-    """
-    Product-GCN.
-    """
-    def __init__(self, c, args):
-        super(ProdGCN, self).__init__(c)
-        print(args.manifold)
-        print(manifolds)
-        self.manifold = getattr(manifolds, args.manifold)()
-        assert args.num_layers > 1
-        dims, acts, self.curvatures = hyp_layers.get_dim_act_curv(args)
-        self.curvatures.append(self.c)
-        hgc_layers = []
-        for i in range(len(dims) - 1):
-            c_in, c_out = self.curvatures[i], self.curvatures[i + 1]
-            in_dim, out_dim = dims[i], dims[i + 1]
-            act = acts[i]
-            hgc_layers.append(
-                hyp_layers.HyperbolicGraphConvolution(
-                    self.manifold, in_dim, out_dim, c_in, c_out, args.dropout, act, args.bias, args.use_att
-                )
-            )
-        self.layers = nn.Sequential(*hgc_layers)
-        self.encode_graph = True
-
-    def encode(self, x, adj):
-        x_hyp = self.manifold.proj(
-                self.manifold.expmap0(self.manifold.proj_tan0(x, self.curvatures[0]), c=self.curvatures[0]),
-                c=self.curvatures[0])
-        return super(ProdGCN, self).encode(x_hyp, adj)
+        return super(Shallow, self).encode(h, adj)
